0e120fe81d133a33221cbadb41c7bd5497c5df0c
Artemii <artemijpotapov1@yandex.ru>
2022-08-06 00:57:45 +0300

    fix: исправить testOk

--- src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -19,7 +19,7 @@ class UserControllerTest {
 
     @BeforeEach
     void createVarsForTests() {
-        user = new User(1, "ololol@ya.ru", "login", "name", LocalDate.of(2022, 8, 6));
+        user = new User(1, "ololol@ya.ru", "login", "name", LocalDate.of(2000, 1, 1));
         userController = new UserController();
     }
 
