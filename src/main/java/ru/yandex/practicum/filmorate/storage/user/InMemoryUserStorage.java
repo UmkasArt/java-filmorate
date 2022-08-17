baeb56a36bf5d09915762ab08eedeedeed2ee359
Artemii <artemijpotapov1@yandex.ru>
2022-08-17 23:14:16 +0300

    Спринт_10 feat: изменить архитектуру приложения

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.storage.user;
+
+import org.apache.catalina.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InMemoryUserStorage implements UserStorage {
+}
